6ce3aad3c224c2396ca95e6a44295187ec9a4272
arnaudDup <arnaud.dupeyrat@gmail.com>
2016-11-27 10:47:52 +0100

    change repository

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // on affiche la page d'acceuil.
-  res.sendFile('Acceuil.html', {root: __dirname + '/../public'});
+  //res.sendfile('./Acceuil.html');
+  res.sendFile('./Acceuil.html', {root: __dirname + '/../public'});
 });
 
 module.exports = router;
