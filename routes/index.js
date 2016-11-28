7e62ec4bcfe02633bfa01a73da21fd667a70d9e4
Arnaud <arnaud.dupeyrat@gmail.com>
2016-11-28 18:58:27 +0100

    change route

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // on affiche la page d'acceuil.
-  //res.sendfile('./Acceuil.html');
-  res.sendFile('./Acceuil.html', {root: __dirname + '/../public'});
+  res.sendFile('acceuil.html', {root: __dirname + './../public'});
 });
 
 module.exports = router;
